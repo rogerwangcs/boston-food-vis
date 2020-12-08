713f5bac342bb6c81f3fbc23a7728fa894119c46
Adri Zhao <zhaocx@bc.edu>
2020-12-07 20:47:07 -0500

    fixedmenu

--- charts/menu.js
+++ charts/menu.js
@@ -1,86 +1,95 @@
 import { getDishes } from "../utils/api.js";
 
-const Menu = async (container) => {
-  const width = 310;
+const Menu = (container) => {
+  const width = 380;
   const height = 370;
-  var margin = { top: 60, right: 10, bottom: 60, left: 50 };
+  var margin = ({ top: 60, right: 10, bottom: 60, left: 50 });
 
   const svg = d3
     .select("#menu")
     .append("svg")
     .attr("width", width)
     .attr("height", height)
+    .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  svg
-    .append("text")
-    .attr("class", "title")
-    .attr("y", -30)
-    .attr("x", width / 2)
-    .style("text-anchor", "middle")
-    .style("fill", "white")
-    .style("font-size", "20px")
-    .text("MENU");
 
   const update = async (restaurantID) => {
     if (!restaurantID) {
       return;
     }
     const dishes = await getDishes(restaurantID);
-    console.log(dishes);
-    svg
-      .append("text")
-      .attr("y", -40)
-      .attr("x", width / 2)
-      .style("text-anchor", "middle")
-      .style("fill", "white")
-      .style("font-size", "14px")
-      .text(dishes[0].dish_name);
 
     svg
       .append("text")
-      .attr("y", -50)
+      .attr("class", "title")
+      .attr("y", -30)
       .attr("x", width / 2)
       .style("text-anchor", "middle")
       .style("fill", "white")
-      .style("font-size", "14px")
-      .text(dishes[1].dish_name);
+      .style("font-size", "20px")
+      .text("MENU");
+
+    svg
+      .selectAll("text")
+      .transition()
+      .style("opacity", 0.0)
+      .duration(500)
+      .remove();
 
     svg
       .append("text")
-      .attr("y", -60)
+      .attr("y", 40)
       .attr("x", width / 2)
       .style("text-anchor", "middle")
       .style("fill", "white")
-      .style("font-size", "14px")
-      .text(dishes[2].dish_name);
+      .style("font-size", "16px")
+      .text("#1 "+dishes[0].dish_name);
 
     svg
       .append("text")
-      .attr("y", -70)
+      .attr("y", 70)
       .attr("x", width / 2)
       .style("text-anchor", "middle")
       .style("fill", "white")
-      .style("font-size", "14px")
-      .text(dishes[3].dish_name);
+      .style("font-size", "16px")
+      .text("#2 " +dishes[1].dish_name);
 
     svg
       .append("text")
-      .attr("y", -80)
+      .attr("y", 100)
       .attr("x", width / 2)
       .style("text-anchor", "middle")
       .style("fill", "white")
-      .style("font-size", "14px")
-      .text(dishes[4].dish_name);
+      .style("font-size", "16px")
+      .text("#3 " +dishes[2].dish_name);
 
     svg
       .append("text")
-      .attr("y", -90)
+      .attr("y", 130)
       .attr("x", width / 2)
       .style("text-anchor", "middle")
       .style("fill", "white")
-      .style("font-size", "14px")
-      .text(dishes[5].dish_name);
+      .style("font-size", "16px")
+      .text("#4 " +dishes[3].dish_name);
+
+    svg
+      .append("text")
+      .attr("y", 160)
+      .attr("x", width / 2)
+      .style("text-anchor", "middle")
+      .style("fill", "white")
+      .style("font-size", "16px")
+      .text("#5 " +dishes[4].dish_name);
+
+    svg
+      .append("text")
+      .attr("y", 190)
+      .attr("x", width / 2)
+      .style("text-anchor", "middle")
+      .style("fill", "white")
+      .style("font-size", "16px")
+      .text("#6 " +dishes[5].dish_name);
+
   };
 
   return {
